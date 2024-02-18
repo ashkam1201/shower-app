de0f79e07d9dd8bc0495e2b0d80cf18a55154f4f
adrian hagjija <ahagjija@openwt.com>
2024-02-18 15:33:12 +0100

    updated responsibe css for mobile

--- showering_app/ClientApp/src/app/login/login.component.ts
+++ showering_app/ClientApp/src/app/login/login.component.ts
@@ -10,6 +10,7 @@ import { Router } from '@angular/router';
 export class LoginComponent {
   email: string = '';
   password: string = '';
+  loginError: string = '';
 
   constructor(private afAuth: AngularFireAuth, private router: Router) { }
 
@@ -20,7 +21,13 @@ export class LoginComponent {
         this.router.navigate(['/']); // Navigate to home or dashboard page after login
       })
       .catch(err => {
-        console.log('Something went wrong:',err.message);
+        console.log('Something went wrong:', err.message);
+        // Update the loginError property with a user-friendly message
+        if (err.code === 'auth/invalid-login-credentials' || err.code === 'auth/user-not-found') {
+          this.loginError = 'Incorrect email or password. Please try again.';
+        } else {
+          this.loginError = 'An error occurred during login. Please try again later.';
+        }
       });
   }
 }
--- showering_app/ClientApp/src/environments/environment.ts
+++ showering_app/ClientApp/src/environments/environment.ts
@@ -1,17 +1,12 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:7250/' // Use HTTPS URL for secure connection
+  apiUrl: 'https://localhost:7250/', // Your API URL
+  firebaseConfig: { // Ensure firebaseConfig is nested within the environment object
+    apiKey: "AIzaSyBSYKXx6TotOUqLwQBTFg-BON3_7TaNsZE",
+    authDomain: "shower-app-auth.firebaseapp.com",
+    projectId: "shower-app-auth",
+    storageBucket: "shower-app-auth.appspot.com",
+    messagingSenderId: "192506494904",
+    appId: "1:192506494904:web:e0f952e5d9fd0d872c594d"
+  }
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
